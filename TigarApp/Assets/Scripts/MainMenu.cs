e444980d009561b7cdd79f08485b0c7b58fb9378
Shiniko <j_culilap1@u.pacific.edu>
2019-03-18 11:00:19 -0700

    Added main menu scene and all four buttons

---
+++ TigarApp/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    public Text testText;
+
+    // Start is called before the first frame update
+    void Start() {
+        testText.text = "No button pressed";
+    }
+
+    // Update is called once per frame
+    void Update() {   
+
+    }
+
+    public void PressedBuildInfo() {
+        testText.text = "Build Info Button has been pressed!";
+    }
+
+    public void PressedLocateBuild() {
+        testText.text = "Locate Building Button has been pressed!";
+    }
+
+    public void PressedViewMap() {
+        testText.text = "View Map Button has been pressed!";
+    }
+
+    public void PressedAbout() {
+        testText.text = "About Button has been pressed!";
+    }
+}
