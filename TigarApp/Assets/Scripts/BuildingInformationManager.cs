aa391d11e285b95bd7d7ebcd77a2f8daf3ba0740
khcanniff <k_canniff@u.pacific.edu>
2019-03-24 16:22:52 -0700

    Done with issue #40

---
+++ TigarApp/Assets/Scripts/BuildingInformationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildingInformationManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OkButton() //if the user clicks the ok button on the building information scene. 
+    {
+        SceneManager.LoadScene("BuildingInformationScanner");
+    }
+}
