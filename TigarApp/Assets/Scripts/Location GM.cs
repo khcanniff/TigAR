2f0a0188e22d15d17d2237683f9223ec2bb7cd82
Neunis <n_nunis@u.pacific.edu>
2019-04-07 01:12:04 -0700

    Connecting the buttons, circle, and lists

--- TigarApp/Assets/Scripts/Location GM.cs
+++ TigarApp/Assets/Scripts/Location GM.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class LocationGM : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //serialized field allows button to be private but viewable in unity editor
+    public Button confirmBttn;
+    public Button undoBttn;
+    public Gameobject BaunCircle;
+    public Gameobject ChambersCircle;
+
     void Start()
     {
-        
+        confirmBttn = GetComponent<Button>();
+        undoBttn = GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChoseBaun()
     {
-        
+        confirmBttn.setActive(true);
+        undoBttn.setActive(true);
+
+        BaunCircle.setActive(true);
+    }
+
+    public void ChoseChambers()
+    {
+        ChambersCircle.setActive(true);
     }
 }
---
+++ TigarApp/Assets/Scripts/LocationGM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationGM : MonoBehaviour
+{
+    public void ChoseBaun()
+    {
+
+    }
+
+    public void ChoseChambers()
+    {
+
+    }
+}
