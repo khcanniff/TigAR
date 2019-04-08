a9c2986ce778b68a68162c14aa8edd9354fa9f61
Jamie Culilap <j_culilap1@u.pacific.edu>
2019-04-08 11:57:34 -0700

    LocateBuilding: functional buttons, circles tied to correct buildings, undo and confirm done

---
+++ TigarApp/Assets/Scripts/LocateBuildingManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocateBuildingManager : MonoBehaviour
+{   
+    public Text ChambersText;
+    public Text BaunText;
+
+    // Specific colors used when choosing words
+    private float r = 0.0f, g = 1.0f, b = 0.0f, a = 1.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void UndoColors() {
+        ChambersText.color = Color.black;
+        BaunText.color = Color.black;
+    }
+
+    public void ChangeChamberColor() {
+        ChambersText.color = Color.green;
+    }
+
+    public void ChangeBaunColor() {
+        BaunText.color = Color.green;
+    }
+}
